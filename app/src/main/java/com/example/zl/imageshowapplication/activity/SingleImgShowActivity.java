1b6d5ec288be1254ee100de8bcf7a642d2f80731
ZhongLeiDev <1942875934@qq.com>
2018-06-12 11:31:15 +0800

    add single image show methods.

---
+++ app/src/main/java/com/example/zl/imageshowapplication/activity/SingleImgShowActivity.java
@@ -0,0 +1,29 @@
+package com.example.zl.imageshowapplication.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.zl.imageshowapplication.R;
+
+/**
+ * Created by ZhongLeiDev on 2018/6/12.
+ */
+
+public class SingleImgShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.singleimgshow_layout);
+
+
+        initView();
+    }
+
+    private void initView(){
+
+
+    }
+
+}
