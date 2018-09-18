94be6aed91a5c4b4c86ff058bdfc726c5a8946cd
ZhongLeiDev <1942875934@qq.com>
2018-09-18 17:21:53 +0800

    start handle search model

--- app/src/main/java/com/example/zl/imageshowapplication/activity/searchresult/SearchResultActivity.java
+++ app/src/main/java/com/example/zl/imageshowapplication/activity/searchresult/SearchResultActivity.java
@@ -1,6 +1,15 @@
 package com.example.zl.imageshowapplication.activity.searchresult;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.zl.imageshowapplication.R;
+import com.example.zl.imageshowapplication.widget.SimpleToolBar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by ZhongLeiDev on 2018/9/18.
@@ -8,4 +17,21 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class SearchResultActivity extends AppCompatActivity {
+
+    @Bind(R.id.simple_toolbar)
+    SimpleToolBar mToolBar;
+    @Bind(R.id.search_tab_layout)
+    TabLayout mTabLayout;
+    @Bind(R.id.search_view_pager)
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_searchresult_activity);
+
+        ButterKnife.bind(this);
+
+    }
+
 }
